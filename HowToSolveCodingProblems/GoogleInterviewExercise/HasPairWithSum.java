67238d1c5aa6c5ab2c1300d7deba9a073349ee3e
JayIssuree <jay_issuree@hotmail.co.uk>
2022-01-10 16:07:59 +0000

    made hashset the compliment instead of duplicating array

--- HowToSolveCodingProblems/GoogleInterviewExercise/HasPairWithSum.java
+++ HowToSolveCodingProblems/GoogleInterviewExercise/HasPairWithSum.java
@@ -2,19 +2,19 @@ import java.util.HashSet;
 
 class HasPairWithSum{
 
-    static int[] testArray = { 9, 4, 3, 4, 5, 6 };
+    static int[] testArray = { 1, 2, 6};
 
     public static void main(String[] args){
-        System.out.println(hasPairWithSum(testArray, 4));
+        System.out.println(hasPairWithSum(testArray, 9));
     };
 
     public static boolean hasPairWithSum(int[] array, int sumValue){
         HashSet<Integer> hashSet = new HashSet<Integer>();
         for(int i = 0; i < array.length; i++){
-            if(hashSet.contains(sumValue - array[i])){
+            if(hashSet.contains(array[i])){
                 return true;
             } else {
-                hashSet.add(array[i]);
+                hashSet.add(sumValue - array[i]);
             };
         };
         return false;
